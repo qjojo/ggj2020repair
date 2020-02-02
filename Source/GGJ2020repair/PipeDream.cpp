5917f4680df63175937d936bde8ec9f60b114f2d
jonah quist <jquist@cranksoftware.com>
2020-02-02 10:19:16 -0500

    working ufunction decoration

---
+++ Source/GGJ2020repair/PipeDream.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PipeDream.h"
+
+// Sets default values
+APipeDream::APipeDream()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APipeDream::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APipeDream::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
